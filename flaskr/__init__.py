74d5abe3d9e7172ad5aeb9c79f0e175f627d2fac
Pipa <pipa@madeup.cz>
2024-11-12 22:34:51 +0100

    initial changes and testing

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,7 @@
 import os
-
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
-    # Create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='secret'
@@ -15,5 +13,9 @@ def create_app():
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    @app.route('/')
+    def home():
+        return render_template('home.html')
 
     return app
