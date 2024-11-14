b63c6745110b8bb8b955191c52367d72900d4b3e
Pipa <pipa@madeup.cz>
2024-11-14 13:43:02 +0100

    cursor tracking attempts

--- flaskr/main/events.py
+++ flaskr/main/events.py
@@ -4,8 +4,14 @@ from flask_socketio import send, emit
 @socketio.on('connect')
 def handle_connect():
     print('Client connected!')
+    emit("connect", broadcast=True)
 
 @socketio.on('docs_change')
 def handle_docs_change(text):
     emit("docs_update", {"new_docs": text}, broadcast=True)
-    
+
+
+
+#@socketio.on('draw_cursor')
+#def handle_draw_cursor(coords):
+#    emit("draw_cursor", {"x": coords['x'], "y": coords['y']}, broadcast=True)
