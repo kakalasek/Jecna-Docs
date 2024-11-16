f7f3c9778c32f84427393d6b470acc69de49409e
Pipa <pipa@madeup.cz>
2024-11-16 15:22:45 +0100

    list of users

--- flaskr/main/events.py
+++ flaskr/main/events.py
@@ -1,15 +1,22 @@
 from .. import socketio
-from flask_socketio import send, emit
+from flask_socketio import emit
+from flask import request
+
+user_list = []
 
 @socketio.on('connect')
-def handle_connect():
-    print('Client connected!')
+def handle_conect():
+    user_list.append(request.sid)
+    emit("update_user_list", {"user_list": user_list}, broadcast=True)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    user_list.remove(request.sid)
+    emit("update_user_list", {"user_list": user_list}, broadcast=True)
 
 @socketio.on('docs_change')
 def handle_docs_change(text):
     emit("docs_update", {"new_docs": text}, broadcast=True)
-
-
 
 #@socketio.on('draw_cursor')
 #def handle_draw_cursor(coords):
